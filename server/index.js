cd5fb78523d5b173d4145e36c19f12d955b2b00e
Brittany Joiner <brittanyjoiner15@gmail.com>
2020-04-01 15:56:50 -0500

    initial commit

---
+++ server/index.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
+
+//middleware
+app.use(cors());
+app.use(express.json()); //req.nbody
+
+//ROUTES//
+
+//create a todo
+
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *", //we're saying to add to table/row the value to put there, and to return everything
+      [description]
+    );
+    res.json(newTodo.rows[0]); // newTodo.rows[0] says to just return the first thing under rows, which is descirption, instead of all the other things that come with it
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//get all todos
+
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//get a todo
+
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params; //this is telling it to look at the parameters
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id
+    ]);
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//update a todo
+
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id= $2",
+      [description, id]
+    );
+
+    res.json("Todo was updated!");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//delete a todo
+
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id
+    ]);
+
+    res.json("To do was deleted");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+app.listen(5000, () => {
+  console.log("server has started on port 5000");
+});
